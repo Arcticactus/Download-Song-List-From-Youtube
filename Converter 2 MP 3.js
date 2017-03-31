a12842a0b796d72273bc7421af0e09aded6c8e83
Arcticactus <Arcticactus@users.noreply.github.com>
2017-03-31 12:15:02 +0100

    Create Converter 2 MP 3.js

---
+++ Converter 2 MP 3.js
@@ -0,0 +1,13 @@
+document.getElementById('urlinput').value="https://www.youtube.com/watch?v=????????????";
+document.getElementsByTagName("button")[1].click();
+
+/*Skips All tags*/
+document.getElementsByTagName("a")[12].click();
+
+/*Download*/
+document.getElementsByTagName("a")[9].click();
+/*convert next*/
+document.getElementsByTagName("span")[0].click();
+
+
+document.getElementsByTagName("a")[9].click();
